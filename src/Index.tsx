e708759bdd4fe5f3e9802f4ea41fe749f0632599
Juan Aquino <juan@riverkan.com>
2023-04-18 16:07:27 -0300

    Sass/Css installed

--- src/Index.tsx
+++ src/Index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import "./Index.scss";
 
 const element : any = document.getElementById("root")
 createRoot(element).render(<div>Hi</div>);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require("path");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
 module.exports = {
   entry: "./src/Index.tsx",
   output: {
@@ -19,9 +22,33 @@ module.exports = {
           loader: "babel-loader",
         },
       },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+      // `...`,
+      new CssMinimizerPlugin(),
     ],
   },
   resolve: {
     extensions: [".tsx", ".ts", ".jsx", ".js"],
   },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "./public/"),
+        },
+      ],
+    }),
+  ],
 };
